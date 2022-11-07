62498b6bdd861e6b868e2283dfe9895775a16d16
Arbazbms <ArbazAhmed.cs19@bmsce.ac.in>
2022-11-07 12:09:27 +0530

    added midtier1 endpoints

---
+++ routes/service.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+let ports={
+  "laptops":3036,
+  "books":3034,
+  "dvds":3035
+}
+
+/* GET users listing. */
+router.get('/:servicename/all/:location?', function (req, res1, next) {
+  let location = req.params.location;
+  let servicename = req.params.servicename;
+  if (location === undefined)
+    location = '';
+  request(`http://localhost:${ports[servicename]}/${servicename}/all/${location}`, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body);
+    res1.json(body);
+  });
+});
+router.get('/:servicename/team', function (req, res1, next) {
+
+  let servicename = req.params.servicename;
+  
+  request(`http://localhost:${ports[servicename]}/${servicename}/team`, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body);
+    res1.json(body);
+  });
+});
+
+module.exports = router;
