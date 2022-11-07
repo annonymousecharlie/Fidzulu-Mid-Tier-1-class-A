2c6ac85e6c55d44ee6e6c8ab3c02500df2e53df5
Arbazbms <ArbazAhmed.cs19@bmsce.ac.in>
2022-11-07 12:36:41 +0530

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,6 +14,8 @@ var port = 3022;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
